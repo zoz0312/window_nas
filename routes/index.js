23f1153d09e65a08d728ed9e5fa7d816459589ef
AJu <zoz0312@naver.com>
2019-08-23 16:42:51 +0900

    Up/Download 기능 구현

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,45 @@
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
+const express = require('express');
+const router = express.Router();
+
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var dir = './';
-  var files = fs.readdirSync(dir); // 디렉토리를 읽어온다
-  console.log(files);
+  const dir = './file_upload';
+  const files = fs.readdirSync(dir); // 디렉토리를 읽어온다
+
+  console.log('files',files);
+
   res.render('index', { 
     title: 'Express',
     files
   });
+});
+
+router.get('/upload', (req, res,) => {
+  res.redirect(301, '/');
+});
+router.post('/upload', (req, res,) => {
+  if (Object.keys(req.files).length == 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  let sampleFile = req.files.sampleFile;
+
+  if( Array.isArray(sampleFile) ){
+    for( let i=0; i<sampleFile.length; i++ ){
+      sampleFile[i].mv('./file_upload/'+sampleFile[i].name), err => {
+        if (err)
+          console.log(res.status(500).send(err));
+      }
+    }
+  } else {
+    sampleFile.mv('./file_upload/'+sampleFile.name), err => {
+      if (err)
+      console.log(res.status(500).send(err));
+    }
+  }
+  res.redirect(301, '/');
 });
 
 module.exports = router;
